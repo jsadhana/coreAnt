6cdd2e610b42410bab306bb87d64fc150a3836cd
076226744 <076226744@LAPTOP-BQVQHD4B>
2023-09-03 07:22:09 +0530

    FirstClass

---
+++ FirstProject/src/com/hcl/test/MyFirstClass.java
@@ -0,0 +1,12 @@
+package com.hcl.test;
+
+public class MyFirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(" ADM DARA ");
+
+	}
+
+}
